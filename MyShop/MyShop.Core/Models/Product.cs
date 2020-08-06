6588a45859a43319f9a42cacc50e312f52fc55d5
Kjetil Mæland <kjetil.maland@gmail.com>
2020-08-06 14:30:55 +0200

    SQL repositories setup

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -14,14 +14,10 @@ namespace MyShop.Core.Models
         [StringLength(20)]
         [DisplayName("Product Name")]
         public string Name { get; set; }
-
         public string Description { get; set; }
         [Range(0,100)]
         public decimal Price { get; set; }
         public string Category { get; set; }
-        public string Image { get; set; }
-
-        
-
+        public string Image { get; set; }      
     }
 }
